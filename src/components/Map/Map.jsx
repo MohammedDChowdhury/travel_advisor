2f7622de6ceda0e1f5ed6409578bcb87f97ecac2
Mohammed <d2018c@gmail.com>
2022-09-08 16:45:38 +0100

    travel advisor - maps currently working

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyC_5g5LHV-afPq-m6v1T-HK7xbE9M-_ZTU" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
